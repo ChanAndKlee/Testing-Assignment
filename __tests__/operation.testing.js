3a50b9e49300af884e62f371f5447c6057c489d8
Ariya Phengphon <ariya.phe@student.mahidol.edu>
2022-11-12 18:02:06 +0700

    Create Unit Testing

--- __tests__/operation.testing.js
+++ __tests__/operation.testing.js
@@ -1,7 +1,73 @@
 const puppeteer = require("puppeteer");
+
 /**
- * Test Case Title: Update student first name on the database
+ * Test Case ID: Fun_01
+ * Test Case Type: Unit Testing
+ * Test Case Title: Check the inserted age
+ * Test Case Description:
+ * - Check the age is greater than or equal to 18
+ * - Check the age is truthiness (null, undefined, defined truthy, falsy)
+*/
+test("Test: Checking the inserted age", () => {
+  const age = 23;
+  expect(age).toBeGreaterThanOrEqual(18);
+  expect(age).not.toBeNull();
+  expect(age).toBeDefined();
+  expect(age).toBeTruthy();
+  expect(age).not.toBeFalsy();
+});
+
+/**
+ * Test Case ID: Fun_02
+ * Test Case Type: Unit Testing
+ * Test Case Title: Check no duplicated name and Firstname starts with "Y" letter
+ * Test Case Description: -
+*/
+describe("Test: No duplicated name and Firstname starts with \"Y\" letter", () => {
+  const existing_firstname = ['Albedo', 'Aloy', 'Amber', 'AratakiItto', 'Barabara',
+  'Beidou', 'Bennett', 'Candace', 'Chongyun', 'Collei',
+  'Cyno', 'Diluc', 'Diona', 'Dori', 'Eula',
+  'Fischl', 'Ganyu', 'Gorou', 'HuTao', 'Jean',
+  'Kazuha', 'Kaeya', 'Ayaka', 'Ayato', 'Keqing',
+  'Klee', 'Kujou', 'Kuki', 'Lisa', 'Nahida'];
+
+  const new_firstname = ['Yae', 'Yelan', 'YunJin'];
+
+  // Check new firstname with the existing
+  // Check new firstname begins with "Y" letter
+  it('\"Does not Match with the existing firstname\" and \"Firstname begins with \"Y\" \"', () => {
+    expect(new_firstname).not.toEqual(expect.arrayContaining(existing_firstname));
+    expect(new_firstname.toString()).toMatch(/^Y/);
+  });
+});
+
+/**
+ * Test Case ID: Fun_03
+ * Test Case Type: Integration Testing
+ * Test Case Title: 
+ * Test Case Description: 
+*/
+
+// (TO DO) Code
+
+
+/**
+ * Test Case ID: Fun_04
+ * Test Case Type: Integration Testing
+ * Test Case Title: 
+ * Test Case Description: 
+*/
+
+// (TO DO) Code
+
+
+/**
+ * Test Case ID: Fun_05
  * Test Case Type: System Testing
+ * Test Case Title: Update student information on the  database
+ * Test Case Description:
+ * - Test the Student Information page by inputting the Student ID, Student Firstname, Student Lastname, Student Age
+ * to update the student information on the database.
 */
 test("Test: Getting the information of student through the user interface.", async () => {
 
@@ -28,7 +94,7 @@ test("Test: Getting the information of student through the user interface.", asy
   page.on("dialog", async (dialog) => {
     await dialog.accept();
   });
-  // Click on "Update" button to search for the student
+  // Click on "Update" button to update the student information
   await page.click("input#update");
 
   // Check all information in term of Object
